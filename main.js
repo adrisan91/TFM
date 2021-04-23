8639cd2603135591a123007744d96f48101bbd28
Adrián Sánchez López <76730053+adriansl756@users.noreply.github.com>
2021-04-23 14:30:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -51,38 +51,3 @@
     });
 
     /*------X----- SCROLLED NAVBAR --------X--------*/
-
-
-    /*------------ MODAL IMGS -----------------*/
-
-      // Get the modal
-      var modal = document.getElementById("myModal");
-
-      // Get the image and insert it inside the modal - use its "alt" text as a caption
-      var img = document.getElementById("imgBrand");
-      var modalImg = document.getElementById("img01");
-      var captionText = document.getElementById("caption");
-  
-      img.onclick = function(){
-        modal.style.display = "flex";
-        modal.style.flexDirection = "column";
-        modal.style.justifyContent = "center";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
-  
-      // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close")[0];
-  
-      // When the user clicks on <span> (x), close the modal
-      modal.onclick = function() { 
-        modal.style.display = "none";
-      }
-
-    /*------X----- MODAL IMGS --------X--------*/
-    
-    /*------------ FADE IN / OUT CONTENT -----------------*/
-      
-
-      
-    /*------X----- FADE IN / OUT CONTENT --------X--------*/
